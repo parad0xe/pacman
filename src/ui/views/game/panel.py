de2aa63936d6dfbe560c60a53225f458933befbf
parad0xe <parad0xe@protonmail.com>
2026-05-19 13:24:45 +0200

    redesign ui codebase

--- src/ui/views/game/panel.py
+++ src/ui/views/game/panel.py
@@ -19,7 +19,11 @@ class GamePanel(UIElementGroup):
         super().__init__(**kwargs)
         self._on_game_over = on_game_over
         self._running = True
+
         self.game = PacmanMock()
+
+        self._super_pacgum_visible: bool = True
+        self._acc_dt: float = 0.0
 
     def _update_impl(self, dt: float) -> None:
         super()._update_impl(dt)
@@ -31,6 +35,8 @@ class GamePanel(UIElementGroup):
         if self.game.is_over:
             self._on_game_over()
             self._running = False
+
+        self._acc_dt += dt
 
     def _render_impl(self) -> None:
         super()._render_impl()
@@ -55,30 +61,61 @@ class GamePanel(UIElementGroup):
             (self.boxes.content_box.height - rows * cell_size) / 2.0
         )
 
+        pacgums = self.game.stage.pacgums
+
         for y in range(rows):
             for x in range(cols):
                 cell = board[y][x]
-                cx = start_x + x * cell_size
-                cy = start_y + y * cell_size
-                next_x = cx + cell_size
-                next_y = cy + cell_size
+                sx = start_x + x * cell_size
+                sy = start_y + y * cell_size
+                next_x = sx + cell_size
+                next_y = sy + cell_size
+                cx = sx + cell_size / 2
+                cy = sy + cell_size / 2
 
                 if cell & 0x1:
-                    pr.draw_line(
-                        int(cx), int(cy), int(next_x), int(cy), pr.BLUE
+                    pr.draw_line_v(
+                        pr.Vector2(sx, sy),
+                        pr.Vector2(next_x, sy),
+                        pr.BLUE,
                     )
                 if cell & 0x2:
-                    pr.draw_line(
-                        int(next_x), int(cy), int(next_x), int(next_y), pr.BLUE
+                    pr.draw_line_v(
+                        pr.Vector2(next_x, sy),
+                        pr.Vector2(next_x, next_y),
+                        pr.BLUE,
                     )
                 if cell & 0x4:
-                    pr.draw_line(
-                        int(cx), int(next_y), int(next_x), int(next_y), pr.BLUE
+                    pr.draw_line_v(
+                        pr.Vector2(sx, next_y),
+                        pr.Vector2(next_x, next_y),
+                        pr.BLUE,
                     )
                 if cell & 0x8:
-                    pr.draw_line(
-                        int(cx), int(cy), int(cx), int(next_y), pr.BLUE
+                    pr.draw_line_v(
+                        pr.Vector2(sx, sy),
+                        pr.Vector2(sx, next_y),
+                        pr.BLUE,
                     )
+
+                if pacgums[y][x] > 0:
+                    is_corner = ((x == 0 and y == 0) or
+                                 (x == cols - 1 and y == 0) or
+                                 (x == 0 and y == rows - 1) or
+                                 (x == cols - 1 and y == rows - 1))
+                    color = pr.Color(180, 180, 10, 200)
+                    if is_corner:
+                        if self._super_pacgum_visible:
+                            pr.draw_circle_v(pr.Vector2(cx, cy), 10, color)
+                        else:
+                            color = pr.Color(180, 180, 10, 50)
+                            pr.draw_circle_v(pr.Vector2(cx, cy), 10, color)
+                    else:
+                        pr.draw_circle_v(pr.Vector2(cx, cy), 3, color)
+
+        if self._acc_dt > 0.8:
+            self._super_pacgum_visible = not self._super_pacgum_visible
+            self._acc_dt = 0.0
 
         self._draw_entity(
             self.game.stage.player.pos,
